f0540e123dd2649cf5d85ea176942fc80ceda0e9
GIlberto Pacheco Gallegos <gilpgnet@gmail.com>
2019-01-22 07:44:58 -0600

    Update logcat.bat

--- logcat.bat
+++ logcat.bat
@@ -1,7 +1,5 @@
 @echo off
 rem Permite ver la bitácora del dispositivo conectado. Lánzalo en una ventana
 rem independiente antes de ejecutar la aplicación.
-PATH=C:\AndroidSdk\platform-tools;%PATH%
 cd /d %0\..
 call adb logcat > logcat.txt
-pause
