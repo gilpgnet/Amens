b0d3cea9db51dbeef5544a4045ba6252ef9d2e1e
GIlberto Pacheco Gallegos <gilpgnet@gmail.com>
2019-01-22 08:40:50 -0600

    Delete logcat.bat

--- logcat.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Permite ver la bitácora del dispositivo conectado. Lánzalo en una ventana
-rem independiente antes de ejecutar la aplicación.
-cd /d %0\..
-call adb logcat > logcat.txt
